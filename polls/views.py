ff7601563f1cb975c376aa926d7501269edb91de
lucasmoriita <lucasmnova@gmail.com>
2020-03-20 16:30:31 -0300

    modify detail method and add the relate template html

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
 from .models import Question
 
@@ -13,6 +13,12 @@ def index(request):
 
 
 def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', context={'question': question})
+
     return HttpResponse("You're looking at question %s." % question_id)
 
 
